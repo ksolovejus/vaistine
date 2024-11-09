071e53d8b5fb48ba1983b8e1405d58f43e378abb
ksolovejus <solovejuskarolis@gmail.com>
2024-11-09 21:40:05 +0200

    Added DB with table nereceptiniai_vaistai

--- add_data_to_db.py
+++ add_data_to_db.py
@@ -8,3 +8,26 @@ print(vaistai[0].get("vodka", "suka bleat is not detected"))
 for v in vaistai[:5]:
     name = v.get("name", "")
     print(f"Name: {name[:10]} || Price: {v.get('price')} || Discount: {v.get('discount')}")
+
+conn = sqlite3.connect("duombaze.db") # (automatically creates if not exist)
+cursor = conn.cursor()
+
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS nereceptiniai_vaistai (
+    id INTEGER PRIMARY KEY,
+    name TEXT NOT NULL,
+    price REAL NOT NULL,
+    discount BOOLEAN NOT NULL             
+)
+""")
+
+for v in vaistai[:5]:
+    cursor.execute(
+        "INSERT INTO nereceptiniai_vaistai (name, price, discount) VALUES (?, ?, ?)",
+        (v.get("name"), v.get("price"), v.get("discount"))
+    )
+
+conn.commit()
+
+# cursor.execute("DELETE FROM nereceptiniai_vaistai")
+# conn.commit()
